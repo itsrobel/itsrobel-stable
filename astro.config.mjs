48588ca06f02121fad1fdefa09c04a34428d8e12
robel schwarz <itsrobel.schwarz@gmail.com>
2024-05-09 00:45:41 -0400

    first

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { remarkReadingTime } from "./src/utils/remark-reading-time.ts";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import svelte from "@astrojs/svelte";
+
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astro-theme-cody.netlify.app",
+  integrations: [mdx(), sitemap(), tailwind(), svelte(), solidJs()],
+  // markdown: {
+  //   remarkPlugins: [remarkReadingTime],
+  //   syntaxHighlight: "shiki",
+  // shikiConfig: {
+  // // Choose from Shiki's built-in themes (or add your own)
+  // // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+  // theme: "andromeeda",
+  // theme: "dark-plus",
+  // theme: "slack-dark",
+  // theme: "material-theme-darker",
+  // themes: {
+  //   light: "github-light",
+  //   dark: "github-dark",
+  // },
+  // },
+  // },
+  output: "hybrid",
+  adapter: vercel()
+});
---
+++ src/consts.ts
@@ -0,0 +1,86 @@
+// This is your config file, place any global data here.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+type Config = {
+  title: string;
+  description: string;
+  lang: string;
+  profile: {
+    author: string;
+    description?: string;
+  };
+};
+
+type SocialLink = {
+  icon: string;
+  handle: string; // for accessibility
+  friendlyName: string; // for accessibility
+  link: string;
+};
+
+export const siteConfig: Config = {
+  title: "The Robellion",
+  description: "",
+  lang: "en-US",
+  profile: {
+    author: "Robel Schwarz",
+    description: "subjectively objectively always correct",
+  },
+};
+
+/** 
+  These are you social media links. 
+  It uses https://github.com/natemoo-re/astro-icon#readme
+  You can find icons @ https://icones.js.org/
+*/
+export const socialLinks: Array<SocialLink> = [
+  {
+    icon: "mdi:github",
+    friendlyName: "Github",
+    link: "https://github.com/itsrobel",
+    handle: "itsrobel",
+  },
+  {
+    icon: "mdi:instagram",
+    friendlyName: "Instagram",
+    link: "https://www.instagram.com/the_robellion/",
+    handle: "the_robellion",
+  },
+  {
+    icon: "mdi:linkedin",
+    friendlyName: "LinkedIn",
+    link: "https://www.linkedin.com/in/robel-schwarz/",
+    handle: "robel-schwarz",
+  },
+  {
+    icon: "mdi:email",
+    friendlyName: "email",
+    link: "mailto:itsrobel.schwarz@gmail.com",
+    handle: "itsrobel.schwarz",
+  },
+
+  // {
+  //   icon: "mdi:rss",
+  //   friendlyName: "rss",
+  //   link: "/rss.xml",
+  // },
+];
+
+export const NAV_LINKS: Array<{ title: string; path: string }> = [
+  // {
+  //   title: "Home",
+  //   path: "/",
+  // },
+  {
+    title: "About",
+    path: "/blog/void",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Projects",
+    path: "/projects",
+  },
+];
